53fdd11dc1aaf8a8985dc04a5a842255fa1c68e8
ferisjuan <feris.juan@gmail.com>
2019-10-28 13:46:03 -0500

    Made small changes, made it responsible

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import categories from './categories/categories';
 
@@ -9,11 +11,17 @@ import categories from './categories/categories';
   encapsulation: ViewEncapsulation.None,
 })
 export class MenuComponent implements OnInit {
-  public categories: {};
+  isMenuVisible: boolean;
+  categories: {};
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.categories = categories;
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((ev: NavigationEnd) => {
+        this.isMenuVisible = ev.url === '/products' ? true : false;
+      });
   }
 }
